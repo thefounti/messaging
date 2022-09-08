e937091e133a6a14bea86791de2d21e5545e0c78
Victor F. Galán <elfounti1983@gmail.com>
2022-09-08 18:25:06 +0200

    Comienzo de lista de fotos para enviar en lugar de keyboard 274(280)

---
+++ components/Grid.js
@@ -0,0 +1,41 @@
+
+import PropTypes from 'prop-types';
+import { Dimensions, FlatList, PixelRatio, StyleSheet } from 'react-native';
+
+export default Grid = (props) => {
+
+    const renderGridItem = (info) => {
+        const { index } = info;
+        const { renderItem, numColumns, itemMargin } = props;
+
+        const { width } = Dimensions.get('window');
+
+        const size = PixelRatio.roundToNearestPixel(
+            (width - itemMargin * (numColumns - 1)) / numColumns,
+        )
+
+        ///MIRAMOS DE NO PONER MARGINLEFT AL PRIMERO DE LAIZQUIERDA
+        const marginLeft = index % numColumns === 0 ? 0 : itemMargin;
+
+        ///MIRAMOS DE NO PONER MARGINTOP A LOS DE LA PRIMERA COLUMNA
+        const marginTop = index < numColumns ? 0 : itemMargin;
+
+        renderItem({ ...info, size, marginLeft, marginTop });
+    }
+
+    return (
+        <FlatList {...props} renderItem={renderGridItem} />
+    )
+
+}
+
+Grid.propTypes = {
+    renderItem: PropTypes.func.isRequired,
+    numColumns: PropTypes.number,
+    itemMargin: PropTypes.number,
+}
+
+Grid.defaultProps = {
+    numColumns: 4,
+    itemMargin: StyleSheet.hairlineWidth,
+}
